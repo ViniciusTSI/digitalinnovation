7ba35bbe721460c03fa34f63cb8bccc1e09d624a
ViniciusTSI <carlosviniciustsi@outlook.com>
2021-07-18 18:03:28 -0300

    Created tests and Report printer

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioAnalista.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digionebank.testes
+
+fun main() {
+    val vicente = Analista("Vicente Carlos", "456.456.456-45", 2000.0)
+
+    ImprimeRelatorioFuncionario.imprime(vicente)
+}
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digionebank.testes
+
+fun main() {
+    val maria = Gerente("Maria Luiza", "789.789.789-78", 5000.0)
+
+    ImprimeRelatorioFuncionario.imprime(maria)
+}
