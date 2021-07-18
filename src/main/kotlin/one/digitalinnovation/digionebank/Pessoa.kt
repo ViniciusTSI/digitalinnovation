e758c13d86c89921f02c66e87813b1e4239d3426
ViniciusTSI <carlosviniciustsi@outlook.com>
2021-07-18 17:52:41 -0300

    Updated to make Person and Employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,17 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Vinicius"
-    var cpf: String = "123.123.123-11"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val vinicius = Pessoa()
-
-    println(vinicius.pessoaInfo())
-}
+abstract class Pessoa(
+    var nome: String,
+    var cpf: String
+)
