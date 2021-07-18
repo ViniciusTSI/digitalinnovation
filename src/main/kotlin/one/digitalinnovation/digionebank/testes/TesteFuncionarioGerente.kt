aafbace0073209f1136bfe2e17c66684b56a6deb
ViniciusTSI <carlosviniciustsi@outlook.com>
2021-07-18 18:20:47 -0300

    Created Final Resources

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,7 +3,9 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val maria = Gerente("Maria Luiza", "789.789.789-78", 5000.0)
+    val maria = Gerente("Maria Luiza", "789.789.789-78", 5000.0, "senha123")
 
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
