ce554179174fc13c0f2286d0e3e086249b9c1ca5
ViniciusTSI <carlosviniciustsi@outlook.com>
2021-07-18 17:29:56 -0300

    Updated to talk about Class Members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,12 +4,14 @@ class Pessoa {
     var nome: String = "Vinicius"
     var cpf: String = "123.123.123-11"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val vinicius = Pessoa()
 
-    println(vinicius)
-    println(vinicius.nome)
-    println(vinicius.cpf)
+    println(vinicius.pessoaInfo())
 }
